19a860401ba38e4d7edeb719e6266d980570345d
MickMac <nick.oliinyk@gmail.com>
2024-09-03 13:07:42 +0200

    Fix compose version

--- app/src/main/java/com/example/crowncompose/ui/composables/Screens.kt
+++ app/src/main/java/com/example/crowncompose/ui/composables/Screens.kt
@@ -2,7 +2,12 @@ package com.example.crowncompose.ui.composables
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -97,7 +102,14 @@ val detailsItems = listOf(
     "Dancing floor",
     "Drink party",
     "Drugs alert",
-    "Best bar choice"
+    "Best bar choice",
+    "--Third Header",
+    "Extra code",
+    "Alcohol policy",
+    "Dancing floor",
+    "Drink party",
+    "Drugs alert",
+    "Best bar choice",
 )
 
 val detailsItemList = List<DetailsItem>(detailsItems.size) { pos ->
